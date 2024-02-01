0ddd948f9f6fa91ef5f8bef1ee94454453992a86
Ali Raza <alirazaasif2511@gmail.com>
2024-02-01 17:55:45 +0600

    create api and back end

---
+++ models/userModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+  },
+  { timestamps: true }
+);
+const User = mongoose.model("User", UserSchema);
+User.createIndexes();
+module.exports = User;
